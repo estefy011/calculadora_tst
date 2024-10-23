dd528fc411bfd909bcf567364c8a9155fc1a0fc4
Estefania <jadira.vinueza@udla.edu.ec>
2024-10-22 20:25:07 -0500

    Hola

--- tests/test_module2.py
+++ tests/test_module2.py
@@ -1,9 +1,11 @@
 from module2 import multiply_numbers
 import unittest
 
+# Dos líneas en blanco antes de la definición de clase
 class TestMultiplyNumbers(unittest.TestCase):
     def test_multiply(self):
         self.assertEqual(multiply_numbers(2, 3), 6)
 
+# Dos líneas en blanco después de la definición de clase
 if __name__ == '__main__':
     unittest.main()
