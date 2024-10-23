79e167b7092f8d6de39f1f22872d0e42f5dba308
Estefania <jadira.vinueza@udla.edu.ec>
2024-10-22 20:31:22 -0500

    Cambio

--- tests/test_module1.py
+++ tests/test_module1.py
@@ -2,10 +2,14 @@ from module1 import add_numbers
 import unittest
 
 # Asegúrate de tener dos líneas en blanco aquí
+
+
 class TestAddNumbers(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add_numbers(1, 2), 3)
 
 # Y dos líneas en blanco aquí también
+
+
 if __name__ == '__main__':
     unittest.main()
